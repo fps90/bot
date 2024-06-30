0ad372522579a0af8a354036bf155ee7ff7353ff
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-30 03:07:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -178,8 +178,8 @@ def display_info(message):
         subject = info.get('subject', 'لم يتم تحديد الموضوع')
         body = info.get('body', 'لم يتم تحديد كليشة الرسالة')
         sleep_time = info.get('sleep_time', 'لم يتم تحديد فترة السليب')
-        bot.send_message(message.chat.id, f"الإيميلات: {email_list}\nالموضوع: {subject}\nكليشة الرسالة: {body}\nفترة السليب: {sleep_time} ثواني")
-         f"صورة مرفقة: {'نعم' if 'image_data' in admin_info else 'لا'}"
+        image_attached = 'نعم' if 'image_data' in info else 'لا'
+        bot.send_message(message.chat.id, f"الإيميلات: {email_list}\nالموضوع: {subject}\nكليشة الرسالة: {body}\nفترة السليب: {sleep_time} ثواني\nصورة مرفقة: {image_attached}")
     else:
         bot.send_message(message.chat.id, "لا توجد معلومات لعرضها.")
 
